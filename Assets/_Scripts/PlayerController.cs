0831a18febd43a9f256949eeb1a97a869a1fcc50
Felipe <felipe.mendez360@gmail.com>
2024-01-26 22:24:38 -0300

    fixes playercontroller y otras cosas jajaj

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -2,30 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
     private Rigidbody playerRb;
     public float speed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        playerRb = GetComponent<Rigidbody>();
-    }
+    Transform cachedCam;
+    Transform t;
+    Vector2 moveInput;
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Awake() {
+        t = this.transform;
+        playerRb = GetComponent<Rigidbody>();
+        cachedCam = Camera.main.transform;
+    }
+    void Update() {
         // Get cursor position
         Ray rayFromCameraToCursor = Camera.main.ScreenPointToRay(Input.mousePosition);
         Plane playerPlane = new Plane(Vector3.up, transform.position);
         playerPlane.Raycast(rayFromCameraToCursor, out float distanceFromCamera);
         Vector3 cursorPosition = rayFromCameraToCursor.GetPoint(distanceFromCamera);
 
-        // Find the new position we'll move to
-        Vector3 inputVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-
-        transform.LookAt(cursorPosition);  // Face the cursor position
-
-        playerRb.velocity = inputVector * speed; // Actually move there
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        Vector3 inputVector = (cachedCam.right.normalized *
+            moveInput.x) + (cachedCam.forward.normalized * moveInput.y);
+        inputVector.y = 0;
+        // VER ESTO PARA QUE MIRE MOUSE,
+        // PERO PIERNAS CALCULEN VECTOR DE DIRECCION PA LA ANIMACION MAS ADELANTE
+        if (moveInput != Vector2.zero) {
+            t.rotation = Quaternion.LookRotation(inputVector);
+        }
+        t.LookAt(cursorPosition);
+        playerRb.MovePosition(t.position + inputVector * speed * Time.deltaTime); // Actually move there
     }
 }
+
