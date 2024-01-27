c432171e60180169e88bda55c7dd0d9c9b64f30d
Felipe <felipe.mendez360@gmail.com>
2024-01-27 03:08:56 -0300

    fix dash, fix normales fbxs, etc

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -3,22 +3,18 @@ using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
+    public bool isGrounded;
+    public bool isDashing;
     private Rigidbody playerRb;
     public float speed;
     //public float jumpForce;
-    public float dashForce;
+    public float dashSpeed;
     public float dashDuration;
-    public KeyCode dashKey = KeyCode.Space;
-    public float dashCd;
-    private float dashCdTimer;
-
-    public float gravityScale;
+    private float dashCdTimer, dashTime;
     Transform cachedCam;
     Transform t;
     Vector2 moveInput;
-    private bool isGrounded;
 
     public GameObject bulletPrefab;
 
@@ -26,16 +22,17 @@ public class PlayerController : MonoBehaviour
     public float bulletHeight;
     float secondsSinceLastShot;
 
+    Vector3 dashDirection;
+    CapsuleCollider capsuleCollider;
 
-    void Awake()
-    {
+    void Awake() {
         t = this.transform;
         playerRb = GetComponent<Rigidbody>();
         cachedCam = Camera.main.transform;
         secondsSinceLastShot = secondsBetweenShots;
+        capsuleCollider = GetComponent<CapsuleCollider>();
     }
-    void Update()
-    {
+    void Update() {
         // Get cursor position
         Ray rayFromCameraToCursor = Camera.main.ScreenPointToRay(Input.mousePosition);
         Plane playerPlane = new Plane(Vector3.up, transform.position);
@@ -46,8 +43,6 @@ public class PlayerController : MonoBehaviour
         moveInput.y = Input.GetAxisRaw("Vertical");
 
         secondsSinceLastShot += Time.deltaTime;
-
-
         // VER ESTO PARA QUE MIRE MOUSE,
         // PERO PIERNAS CALCULEN VECTOR DE DIRECCION PA LA ANIMACION MAS ADELANTE
         /*
@@ -55,38 +50,52 @@ public class PlayerController : MonoBehaviour
             t.rotation = Quaternion.LookRotation(inputVector);
         }
         */
+        if (!isDashing && Input.GetKeyDown(KeyCode.Space) && Time.time > dashCdTimer) {
+            dashCdTimer = Time.time + 2f;
+            dashTime = Time.time + dashDuration;
+            isDashing = true;
+            dashDirection = (cachedCam.right *
+            moveInput.x) + (cachedCam.forward * moveInput.y);
+            dashDirection.y = 0;
+        }
+        if (isDashing) {
+            if (Time.time > dashTime) {
+                isDashing = false;
+            }
+            t.rotation = Quaternion.LookRotation(dashDirection);
+            bool dashObstructed = false;
+            int lm = 1 << 10;
+            if (Physics.Raycast(t.position + (Vector3.up * 0.2f), dashDirection.normalized, capsuleCollider.radius + 0.15f, lm)) {
+                dashObstructed = true;
+            }
+            if (!dashObstructed) {
+                t.position += (dashDirection.normalized * dashSpeed * Time.deltaTime);
+            }
+            return;
+        }
         t.LookAt(cursorPosition);
 
-        if (secondsSinceLastShot >= secondsBetweenShots && Input.GetButton("Fire1"))
-        {
+        if (secondsSinceLastShot >= secondsBetweenShots && Input.GetButton("Fire1")) {
             Instantiate(bulletPrefab, t.position + t.forward + t.up * bulletHeight, t.rotation);
             secondsSinceLastShot = 0;
         }
-
-
     }
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
+        if (isDashing) return;
         Vector3 inputVector = (cachedCam.right *
             moveInput.x) + (cachedCam.forward * moveInput.y);
         inputVector.y = 0;
-        if (moveInput != Vector2.zero)
-        {
+        if (moveInput != Vector2.zero) {
             playerRb.MovePosition(t.position + inputVector.normalized * speed * Time.deltaTime); // Actually move there
         }
     }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.GetComponent<Terrain>() != null)
-        {
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.GetComponent<Terrain>() != null) {
             isGrounded = true;
         }
     }
-    private void OnCollisionExit(Collision other)
-    {
-        if (other.gameObject.GetComponent<Terrain>() != null)
-        {
+    private void OnCollisionExit(Collision other) {
+        if (other.gameObject.GetComponent<Terrain>() != null) {
             isGrounded = false;
         }
     }
